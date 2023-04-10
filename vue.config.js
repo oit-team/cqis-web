fef5853718bd05beeb586d5ef49a3e4a5b4e1b75
邱旭 <uxuip@outlook.com>
2023-04-10 10:57:01 +0800

    build: 打包后移除console和comments

--- vue.config.js
+++ vue.config.js
@@ -30,4 +30,15 @@ module.exports = {
       },
     },
   },
+  chainWebpack: (config) => {
+    config.optimization.minimizer('terser').tap((args) => {
+      args[0].terserOptions.output = {
+        comments: false,
+      }
+      args[0].terserOptions.compress = {
+        drop_console: true,
+      }
+      return args
+    })
+  },
 }
